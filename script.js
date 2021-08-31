8d0553b3e2d15c3d5ec5a21ca9456cceb2bbaf3f
Jingsi <jingsi.zhou@utexas.edu>
2021-08-31 18:11:43 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,50 @@
+/*
+console.log('Hello World :3');
+
+var cockSucker = 'Poo';
+console.log(cockSucker);
+
+var anusSmeller = 'Pee';
+var age = 69;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+job = 'Teacher';
+console.log(job);
+
+// single line comment
+
+/* multiline comment
+    multiline comment
+
+*/
+
+
+// Variable mutation and type conversion
+
+var firstName = 'John';
+var age = 28;
+console.log(firstName + ' ' + age);
+
+//JS always converts types of vars when necessary
+
+var job, isMarried;
+job = 'teacher';
+//isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// Variable mutation
+age = 'twenty eight';
+job = 'driver'
+
+var booleanlol = true;
+while (true) {
+alert('EAT MY ASS');
+}
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName)
